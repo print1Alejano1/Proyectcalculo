dd5e0e4799c6618dc1bc6b4c7094f2a049760b9a
bpedrazav <bpedraza2026@alu.uct.cl>
2026-06-03 22:07:55 -0400

    creacion de la ventana

--- proyectcalculeishion.py
+++ proyectcalculeishion.py
@@ -4,3 +4,8 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import math
 import sympy as sp
+
+#creamos la ventana principal
+programa_calculo = ctk.CTk()    
+programa_calculo.title("EID-CALCULO-LIMITES")
+programa_calculo.geometry("1080x720")
