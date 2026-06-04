7cb0bb328afef4f6c9b700164f2ffa3861056792
bpedrazav <bpedraza2026@alu.uct.cl>
2026-06-03 22:05:34 -0400

    Importar librerias
    Added necessary imports for GUI and plotting.

--- proyectcalculeishion.py
+++ proyectcalculeishion.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+#importamos las librerias necesarias
+import customtkinter as ctk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import math
+import sympy as sp
