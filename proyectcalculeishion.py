e8eed60aa256c9a6cb5f8d722fb653ac4b8c2f96
bpedrazav <bpedraza2026@alu.uct.cl>
2026-06-04 21:13:22 -0400

    Actualización de funciones

--- proyectcalculeishion.py
+++ proyectcalculeishion.py
@@ -1,29 +1,38 @@
-#Iportamos las librerias necesarias
-import customtkinter as ctk #aqui tambien estamos dejando abreviado a ctk, y eso vamos a suar cuando queramos usar algo con el custom 
+#Importamos las librerias necesarias
+import customtkinter as ctk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import math
 import sympy as sp
 
-#creamos la ventana principal
-ctk.set_appearance_mode("System") #aqui nos saldra del color del modo k este el equipo, claro, oscuro
-ctk.set_default_color_theme("blue") #y usamos 
-
-
-programa_calculo = ctk.CTk()    
+#creamos la ventana principal y su aspecto en general (dependiendo del tema del computador, 
+#es decir si esta en modo claro o modo oscuro)
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+programa_calculo = ctk.CTk()
 programa_calculo.title("EID-CALCULO-LIMITES")
 programa_calculo.geometry("1080x720")
 
-#crear la funcion que sera llamada cuando el bton se llegue a presionar
-def calculowe():
-    print("el ingeniero aun esta cocinando esperen")
-
-
-#crear el boton, 
-button = ctk.CTkButton(master=programa_calculo, text="Calcular", command=calculowe)
-button.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
 
 
-#para que no se cierre el programa
-programa_calculo.mainloop()
+#_____________________________________creación de las funciones del programa______________________________________________
+#función limpiar texto
+def limpiar():
+    ingresar_datos.delete("1.0", "end")
+    ingresar_tendencia.delete("1.0", "end")
+
+#funcion coseno
+def coseno():
+    ingresar_datos.insert("end","cos(x)") 
+
+#funcion seno
+def seno():
+    ingresar_datos.insert("end","sen(x)")
+
+#funcion tangente
+def tangente():
+    ingresar_datos.insert("end","tan(x)")
+
+def calcular():
+    print("NO TOI NI AHI CON EL LIMITE")
